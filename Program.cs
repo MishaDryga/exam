012fdf47baa9d6d5529eea7c550ff4c059ee74ca
MishaDryga <70898161+MishaDryga@users.noreply.github.com>
2021-12-20 11:31:53 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (weekNum % 2 == 0)
+            {
+                Console.WriteLine("четное");// четное
+            }
+            else
+            {
+                Console.WriteLine("не четное");// не четное
+            }
+        }
+        DateTime dt = DateTime.Parse(Console.ReadLine());
+        int weekNum = GetWeekNumber(dt);
+
+        public int GetWeekNumber(DateTime dt)
+        {
+            CultureInfo ciCurr = CultureInfo.CurrentCulture;
+            int weekNum = ciCurr.Calendar.GetWeekOfYear(dt, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return weekNum;
+        }
+    }
+}
